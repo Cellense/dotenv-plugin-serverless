af695c037deeae7e42c3e3891575fde091e094ee
Matus Congrady <m.congrady@gmail.com>
2018-11-02 10:53:12 +0100

    make all .env files optional

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const { parse } = require('dotenv');
 const { fatal, star } = require('signale');
-const { readFileSync } = require('fs')
+const { readFileSync, existsSync } = require('fs');
 
 function setEnvVariables(envVariables) {
   for (var key in envVariables) {
-    process.env[key] = envVariables[key]
+    process.env[key] = envVariables[key];
   }
 }
 
 module.exports = class ServerlessPlugin {
   constructor(serverless) {
     const stage = serverless.processedInput.options.stage;
-    if (!stage) throw new Error('Dotenv plugin serverless requires --stage option')
+    if (!stage) throw new Error('Dotenv plugin serverless requires --stage option');
     try {
       const stageSpecificDotenvPath = stage === 'production' || stage === 'staging' ? `.env.${stage}` : '.env.dev';
-      const commonVariables = parse(readFileSync('.env.common'));
-      const stageSpecificVariables = parse(readFileSync(stageSpecificDotenvPath));
-      const customVariables = parse(readFileSync('.env'));
-      
+      const commonVariables = existsSync('.env.common') && parse(readFileSync('.env.common'));
+      const stageSpecificVariables =
+        existsSync(stageSpecificDotenvPath) && parse(readFileSync(stageSpecificDotenvPath));
+      const customVariables = existsSync('.env') && parse(readFileSync('.env'));
+
       setEnvVariables(commonVariables);
       setEnvVariables(stageSpecificVariables);
       setEnvVariables(customVariables);
 
-      star(`Loading ENV variables from .env.common, ${stageSpecificDotenvPath} and .env`);
+      star(`Loading ENV variables from: ${commonVariables ? '.env.common' : ''} ${stageSpecificVariables ? stageSpecificDotenvPath : ''} ${customVariables ? '.env' : ''}`);
     } catch (e) {
-      fatal(`Could not load env variables!`)
-      fatal(e)
+      fatal(`Could not load env variables!`);
+      fatal(e);
     }
   }
-}
+};
